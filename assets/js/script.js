176aa6e536fcb9a47b3942943d73ead12caf2356
sanjit018 <sanjitgorai716@gmail.com>
2025-11-10 22:49:39 +0530

    topbar and navbar completed

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  $("#menubtn").click(function () {
+    // If #mobiledetail is visible, close it first
+    if ($("#mobiledetail").is(":visible")) {
+      $("#mobiledetail")
+        .stop(true, true)
+        .animate({ width: "toggle", opacity: "toggle" }, 400, function () {
+          // After mobiledetail closes, open navmobile
+          $("#navmobile")
+            .stop(true, true)
+            .animate({ width: "toggle", opacity: "toggle" }, 500);
+        });
+    } else {
+      // Otherwise, just toggle navmobile
+      $("#navmobile")
+        .stop(true, true)
+        .animate({ width: "toggle", opacity: "toggle" }, 500);
+    }
+  });
+
+  $("#sidebtn").click(function () {
+    // If #navmobile is visible, close it first
+    if ($("#navmobile").is(":visible")) {
+      $("#navmobile")
+        .stop(true, true)
+        .animate({ width: "toggle", opacity: "toggle" }, 400, function () {
+          // After navmobile closes, open mobiledetail
+          $("#mobiledetail")
+            .stop(true, true)
+            .animate({ width: "toggle", opacity: "toggle" }, 500);
+        });
+    } else {
+      // Otherwise, just toggle mobiledetail
+      $("#mobiledetail")
+        .stop(true, true)
+        .animate({ width: "toggle", opacity: "toggle" }, 500);
+    }
+  });
+  
+})
